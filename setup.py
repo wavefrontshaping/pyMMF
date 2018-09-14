bdd0f874ffcfff2601da308965e6002e4a1043c2
sebastien.popoff@espci.fr <sebastien.popoff@espci.fr>
2018-09-14 14:44:36 +0200

    add sertup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+import os
+
+
+long_description='Please read the documentation on Github.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+
+setup(name='pyMMF',
+    version='0.1',
+    description='Multimode optical fiber simulation package.',
+    author='Sebastien M. Popoff',
+    author_email='sebastien.popoff@espci.psl.eu',
+    url='https://www.wavefrontshaping.net',
+    license = 'MIT',
+    py_modules = ['pyMMF'],
+    long_description=long_description,
+    classifiers=[
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+    ],
+    )
