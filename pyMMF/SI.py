e81ab8d723f380a46ad728d5adfd1ee390dca6b9
sebastien.popoff@espci.fr <sebastien.popoff@espci.fr>
2019-02-21 15:56:12 +0100

    change root finding method for semi-analytical SI fibers

--- pyMMF/SI.py
+++ pyMMF/SI.py
@@ -9,7 +9,8 @@ Created on Mon Feb  4 12:02:57 2019
 from scipy.special import jv,kv,iv
 import matplotlib.pyplot as plt
 
-from scipy.signal import find_peaks_cwt
+#from scipy.signal import find_peaks_cwt
+#import peakutils
 
 #try:
 #    from peakutils import indexes 
@@ -18,18 +19,30 @@ from scipy.signal import find_peaks_cwt
 #    from scipy.signal import find_peaks_cwt
 #    find_peaks = lambda x: find_peaks_cwt(x, np.arange(0.1,3,0.1))
 
-#import logging
+
+
 import time
-#from scipy.linalg import expm
 import numpy as np
 from .modes import Modes
-#from .core import logger
-from .logger import get_logger, handleException 
+from .logger import get_logger
+from scipy.optimize import root
 
 logger = get_logger(__name__)
 
+def _root_guesses(f,a,b,dx):
+    x1 = a; f1 = f(a)
+    x2 = a + dx; f2 = f(x2)
+    roots = []
+    while x1 < b:
+        if f1*f2 < 0 and f1*f2 > -1.:
+            roots.append(.5*(x1+x2))
+        x1 = x2; f1 = f2
+        x2 = x1 + dx; f2 = f(x2)
+    return roots
 
-def findPropagationConstants(wl,indexProfile):
+
+
+def findPropagationConstants(wl,indexProfile, tol=1e-9):
     '''
     Find the propagation constants of a step index fiber by numerically finding the sollution of the
     scalar dispersion relation [1]_ [2]_.
@@ -77,60 +90,48 @@ def findPropagationConstants(wl,indexProfile):
     # Based on the dispersion relation,l eq. 3.74
 
     # The normalized frequency, cf formula 3.20 of the book
-    v=(2*np.pi/lbda*NA*a)
-    
-    # u and w are the normalized transverse wave numbers (3.16a and 3.16b)
-    u=np.array(np.linspace(np.spacing(1),v-np.spacing(10),2e4))
-
-    w=np.sqrt(v**2-u**2)
-    
-    peaks = [0]
+    v=(2*np.pi/lbda*NA*a)   
+   
+    roots = [0]
     m=0
     modes = Modes()
     
     
+    
+    
 
-    while(len(peaks)>0):
+    while(len(roots)>0):
         
-        # Function that is 1/abs(dispersion relation), based on the dispersion relation,l eq. 3.74 of "Fundamentals of optical wavegudies" by H. Okamoto
-        # the number and positions of the maximum gives the modes of the fiber
-#        if m == 0:
-#            Function = 1/np.abs(jv(0,u)/(u*jv(1,u))-kv(0,w)/(w*kv(1,w)))
-#        print(u)
-        
-#        else:
-#        Function = (u*jv(m-1,u))+kv(m,w)/(w*kv(m-1,w))
-        Function = 1./(np.spacing(1)+np.abs(jv(m,u)/(u*jv(m-1,u))+kv(m,w)/(w*kv(m-1,w))))
-  
-#        peaks = peakutils.indexes(Function, thres=0.02/max(Function), min_dist=10)
+        def root_func(u):
+            w=np.sqrt(v**2-u**2)
+            return jv(m,u)/(u*jv(m-1,u))+kv(m,w)/(w*kv(m-1,w))
+               
+        guesses = _root_guesses(root_func,np.spacing(10),v-np.spacing(10),v*1e-4)
+        froot = lambda x0: root(root_func,x0,tol = tol)
+        sols = list(map(froot,guesses))
+        roots = [s.x for s in sols if s.success]
+      
+        # remove solution outside the valid interval, round the solutions and remove duplicates
+        roots = np.unique([np.round(r/tol)*tol for r in roots if (r > 0 and r<v)]).tolist()
 
-        peaks = find_peaks_cwt(Function, np.arange(0.1,3,0.1))
-        
-#        plt.figure()
-#        plt.plot(u,Function)
-##        plt.hold(True)
-#        plt.plot(u[peaks],Function[peaks],'ko')
         
         
-        if(len(peaks) > 0):
+        if(len(roots) > 0):
            
             degeneracy = 1 if m == 0 else 2
-            
-            modes.betas = np.concatenate((modes.betas, [np.sqrt((2*np.pi/lbda*n1)**2-(u[p]/a)**2) for p in peaks]*degeneracy))
-            modes.u = np.concatenate((modes.u,[u[p] for p in peaks]*degeneracy))
-            modes.w = np.concatenate((modes.w,[np.sqrt(v**2-u[p]**2) for p in peaks]*degeneracy))
-            modes.number += len(peaks)*degeneracy
-            modes.m.extend([m]*len(peaks)*degeneracy)
-            modes.l.extend([x+1 for x in range(len(peaks))]*degeneracy)
-#            modes.l.extend(np.arange(len(peaks))+1)
-            for l in range(len(peaks)*degeneracy):
+            modes.betas = np.concatenate((modes.betas, [np.sqrt((2*np.pi/lbda*n1)**2-(r/a)**2) for r in roots]*degeneracy))
+            modes.u = np.concatenate((modes.u,roots*degeneracy))
+            modes.w = np.concatenate((modes.w,[np.sqrt(v**2-r**2) for r in roots]*degeneracy))
+            modes.number += len(roots)*degeneracy
+            modes.m.extend([m]*len(roots)*degeneracy)
+            modes.l.extend([x+1 for x in range(len(roots))]*degeneracy)
+            for l in range(len(roots)*degeneracy):
                 modes.modesList.append(str(m)+','+str(l+1))
             
         
         m+=1
     
     logger.info("Found %g modes is %0.2f seconds." % (modes.number,time.time()-t0))
-#    print(modes.modesList)
     return modes
 
 
