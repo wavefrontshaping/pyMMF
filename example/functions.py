47adc9e8dde15bffb25de5eefc2ed801a9c64138
Your Name <you@example.com>
2021-09-17 12:19:12 +0200

    add GRIN example for radial solver

---
+++ example/functions.py
@@ -0,0 +1,21 @@
+from colorsys import hls_to_rgb
+import numpy as np
+
+def colorize(z, theme = 'dark', saturation = 1., beta = 1.4, transparent = False, alpha = 1.):
+    r = np.abs(z)
+    r /= np.max(np.abs(r))
+    arg = np.angle(z) 
+
+    h = (arg + np.pi)  / (2 * np.pi) + 0.5
+    l = 1./(1. + r**beta) if theme == 'white' else 1.- 1./(1. + r**beta)
+    s = saturation
+
+    c = np.vectorize(hls_to_rgb) (h,l,s) # --> tuple
+    c = np.array(c)  # -->  array of (3,n,m) shape, but need (n,m,3)
+    c = c.swapaxes(0,2) 
+    if transparent:
+        a = 1.-np.sum(c**2, axis = -1)/3
+        alpha_channel = a[...,None]**alpha
+        return np.concatenate([c,alpha_channel], axis = -1)
+    else:
+        return c
