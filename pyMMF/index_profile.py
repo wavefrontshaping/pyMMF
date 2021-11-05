e8d2f3956279cedc83878303216db02310330d3a
HauptMachine\vonGostev <gostev.pavel@physics.msu.ru>
2021-11-05 18:54:02 +0300

    Fix MultiCore index profile. Add Microstructured index profile

--- pyMMF/index_profile.py
+++ pyMMF/index_profile.py
@@ -62,22 +62,27 @@ class IndexProfile():
         radialFunc = lambda r: n1 if r < a else n2
 
         self.initFromRadialFunction(radialFunc)
-        
+
     def initStepIndexMultiCore(
             self, n1: float = 1.45, a: float = 1., delta: float = 0.039,
             dims: int = 4, layers: int = 1,
-            NA: float = 0.4, core_offset: float = 5):
+            NA: float = 0.4, core_pitch: float = 5,
+            central_core: bool = True):
         n2 = n1 * (1 - delta)
         self.NA = NA
         self.a = a
         self.type = 'SIMC'
         self.n1 = n1
-        core_offset = int(core_offset // self.dh)
-        angles = np.arange(0, 2 * np.pi, 2 * np.pi / dims)
-        radiuses = np.arange(layers + 1) * core_offset
-        cores_coords = [[self.npoints // 2 + int(r * np.sin(t)),
-                         self.npoints // 2 + int(r * np.cos(t))]
-                        for r in radiuses for t in angles]
+        core_pitch = int(core_pitch // self.dh)
+        lgrid = np.arange(0 if central_core else 1, layers + 1)
+        angles = [
+            np.arange(0, 2 * np.pi, 2 * np.pi / dims / lnum) for lnum in lgrid]
+        radiuses = lgrid * core_pitch
+        cores_coords = [[
+            [self.npoints // 2 + int(r * np.sin(t)),
+             self.npoints // 2 + int(r * np.cos(t))]
+            for t in _a] for r, _a in zip(radiuses, angles)]
+        cores_coords = sum(cores_coords, [])
         self.n = np.ones_like(self.R) * n2
         for indxs in cores_coords:
             i, j = indxs
@@ -87,13 +92,13 @@ class IndexProfile():
 
     def initStepIndexConcentric(
             self, n1: float = 1.45, a: float = 1., delta: float = 0.039,
-            NA: float = 0.4, core_offset: float = 5, layers: int = 1):
+            NA: float = 0.4, core_pitch: float = 5, layers: int = 1):
         n2 = n1 * (1 - delta)
         self.NA = NA
         self.a = a
         self.type = 'SIC'
         self.n1 = n1
-        radiuses = np.arange(layers + 1) * core_offset
+        radiuses = np.arange(layers + 1) * core_pitch
 
         def radialFunc(r):
             for r0 in radiuses:
@@ -102,3 +107,19 @@ class IndexProfile():
             return n2
 
         self.initFromRadialFunction(radialFunc)
+
+    def initStepIndexMicrostructured(
+            self, n1: float = 1.45, a: float = 1.,
+            dims: int = 4, layers: int = 1,
+            NA: float = 0.4, core_pitch: float = 5,
+            cladding_radius: float = 25):
+        delta = 1 - n1
+        n2 = np.sqrt(n1**2-NA**2)
+        self.initStepIndexMultiCore(
+            1, a, delta, dims, layers, NA, core_pitch,
+            central_core=0)
+        self.type = 'MSF'
+        self._crop_cladding(n2, cladding_radius)
+
+    def _crop_cladding(self, n2, cladding_radius):
+        self.n[self.R > cladding_radius] = n2
