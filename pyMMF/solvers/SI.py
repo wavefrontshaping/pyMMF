2ca422b3a1a048ddea56ebf6da0f8ccd8b14983a
Sébastien M. Popoff <sebastien.popoff@espci.fr>
2023-06-14 11:47:45 +0200

    format

--- pyMMF/solvers/SI.py
+++ pyMMF/solvers/SI.py
@@ -1,7 +1,7 @@
-'''
+"""
 Solver for step index fiber using analytical expression of the mode profile 
 and solving numerically the analytical dispersion equation.
-'''
+"""
 
 import time
 import numpy as np
@@ -10,108 +10,115 @@ from scipy.special import jv, kn
 
 from ..modes import Modes
 from ..logger import get_logger
+
 logger = get_logger(__name__)
 
 from joblib import Parallel, delayed
 
+
 def solve_SI(indexProfile, wl, **options):
-    degenerate_mode = options.get('degenerate_mode','sin')
-    n_jobs = options.get('n_jobs', -2)
-    modes = findPropagationConstants(wl,indexProfile)
-    modes = associateLPModeProfiles(modes,indexProfile,
-                                    degenerate_mode=degenerate_mode,
-                                    n_jobs=n_jobs)
+    degenerate_mode = options.get("degenerate_mode", "sin")
+    n_jobs = options.get("n_jobs", -2)
+    modes = findPropagationConstants(wl, indexProfile)
+    modes = associateLPModeProfiles(
+        modes, indexProfile, degenerate_mode=degenerate_mode, n_jobs=n_jobs
+    )
     return modes
 
 
-def findPropagationConstants(wl,indexProfile, tol=1e-9):
-    '''
+def findPropagationConstants(wl, indexProfile, tol=1e-9):
+    """
     Find the propagation constants of a step index fiber by numerically finding the solution of the
     scalar dispersion relation [1]_ [2]_.
-    
 
-   
-    
+
+
+
     Parameters
     ----------
     wl : float
-    		wavelength in microns.
-        
+                wavelength in microns.
+
     indexProfile: IndexProfile object
         object that contains data about the transverse index profile.
-        
+
     Returns
     -------
     modes : Modes object
-        Object containing data about the modes. 
-        Note that it does not fill the transverse profiles, only the data about the propagation constants 
+        Object containing data about the modes.
+        Note that it does not fill the transverse profiles, only the data about the propagation constants
         and the mode numbers.
-    
+
     See Also
     --------
         associateLPModeProfiles()
-    
+
     Notes
     -----
-         
-    .. [1]  K. Okamoto, "Fundamentals of optical waveguides" 
+
+    .. [1]  K. Okamoto, "Fundamentals of optical waveguides"
             Academic Press,
             2006
-            
-    .. [2]  S. M. Popoff, "Modes of step index multimode fibers" 
+
+    .. [2]  S. M. Popoff, "Modes of step index multimode fibers"
             http://wavefrontshaping.net/index.php/component/content/article/68-community/tutorials/multimode-fibers/118-modes-of-step-index-multimode-fibers
-            
-    '''
+
+    """
     lbda = wl
     NA = indexProfile.NA
     a = indexProfile.a
     n1 = indexProfile.n1
-    
-    logger.info('Finding the propagation constant of step index fiber by numerically solving the dispersion relation.')
+
+    logger.info(
+        "Finding the propagation constant of step index fiber by numerically solving the dispersion relation."
+    )
     t0 = time.time()
     # Based on the dispersion relation,l eq. 3.74
 
     # The normalized frequency, cf formula 3.20 of the book
-    v=(2*np.pi/lbda*NA*a)   
-   
+    v = 2 * np.pi / lbda * NA * a
+
     roots = [0]
     m = 0
     modes = Modes()
 
-
-    interval = np.arange(np.spacing(10),v-np.spacing(10),v*1e-4)
+    interval = np.arange(np.spacing(10), v - np.spacing(10), v * 1e-4)
     while len(roots):
-        
+
         def root_func(u):
-            w=np.sqrt(v**2-u**2)
-            return jv(m,u)/(u*jv(m-1,u))+kn(m,w)/(w*kn(m-1,w))
-               
+            w = np.sqrt(v**2 - u**2)
+            return jv(m, u) / (u * jv(m - 1, u)) + kn(m, w) / (w * kn(m - 1, w))
+
         guesses = np.argwhere(np.abs(np.diff(np.sign(root_func(interval)))))
-        froot = lambda x0: root(root_func,x0,tol = tol)
+        froot = lambda x0: root(root_func, x0, tol=tol)
         sols = map(froot, interval[guesses])
         roots = [s.x for s in sols if s.success]
-      
+
         # remove solution outside the valid interval, round the solutions and remove duplicates
-        roots = np.unique([np.round(r/tol)*tol for r in roots if (r > 0 and r<v)]).tolist()
+        roots = np.unique(
+            [np.round(r / tol) * tol for r in roots if (r > 0 and r < v)]
+        ).tolist()
         roots_num = len(roots)
 
         if roots_num:
             degeneracy = 1 if m == 0 else 2
-            modes.betas.extend([np.sqrt((2*np.pi/lbda*n1)**2-(r/a)**2) for r in roots]*degeneracy)
-            modes.u.extend(roots*degeneracy)
-            modes.w.extend([np.sqrt(v**2-r**2) for r in roots]*degeneracy)
-            modes.number += roots_num*degeneracy
-            modes.m.extend([m]*roots_num*degeneracy)
-            modes.l.extend([x+1 for x in range(roots_num)]*degeneracy)
+            modes.betas.extend(
+                [np.sqrt((2 * np.pi / lbda * n1) ** 2 - (r / a) ** 2) for r in roots]
+                * degeneracy
+            )
+            modes.u.extend(roots * degeneracy)
+            modes.w.extend([np.sqrt(v**2 - r**2) for r in roots] * degeneracy)
+            modes.number += roots_num * degeneracy
+            modes.m.extend([m] * roots_num * degeneracy)
+            modes.l.extend([x + 1 for x in range(roots_num)] * degeneracy)
 
         m += 1
-    
-    logger.info("Found %g modes in %0.2f seconds." % (modes.number,time.time()-t0))
+
+    logger.info("Found %g modes in %0.2f seconds." % (modes.number, time.time() - t0))
     return modes
 
 
-def calc_mode(modes, idx, degenerate_mode, R, a, TH,
-              Rlessa, Rgreatera):
+def calc_mode(modes, idx, degenerate_mode, R, a, TH, Rlessa, Rgreatera):
     m = modes.m[idx]
     l = modes.l[idx]
     u = modes.u[idx]
@@ -126,52 +133,59 @@ def calc_mode(modes, idx, degenerate_mode, R, a, TH,
     psi = 0
 
     # Non-zero transverse component
-    if degenerate_mode == 'sin':
+    if degenerate_mode == "sin":
         # two pi/2 rotated degenerate modes for m < 0
-        psi = np.pi/2 if degenerated else 0 
+        psi = np.pi / 2 if degenerated else 0
         phase_mult = np.cos(phase + psi)
 
-    elif degenerate_mode == 'exp':
+    elif degenerate_mode == "exp":
         # noticably faster than writing exp(1j*phase)
         phase_mult = np.cos(phase) + 1j * np.sin(phase)
 
-    Et = phase_mult * (jv(m, u/a*R)/jv(m, u)*Rlessa +
-                       kn(m, w/a*R)/kn(m, w)*Rgreatera)
+    Et = phase_mult * (
+        jv(m, u / a * R) / jv(m, u) * Rlessa + kn(m, w / a * R) / kn(m, w) * Rgreatera
+    )
     mode = Et.ravel().astype(np.complex64)
-    mode /= np.sqrt(np.sum(np.abs(mode)**2))
+    mode /= np.sqrt(np.sum(np.abs(mode) ** 2))
     return mode
 
 
-def associateLPModeProfiles(modes, indexProfile, degenerate_mode='sin',
-                            n_jobs=-2):
-    '''
+def associateLPModeProfiles(modes, indexProfile, degenerate_mode="sin", n_jobs=-2):
+    """
     Associate the linearly polarized mode profile to the corresponding constants found solving the analytical dispersion relation.
     see: "Weakly Guiding Fibers" by D. Golge in Applied Optics, 1971
-    '''
-    
-    assert(not modes.profiles)
-    assert(degenerate_mode in ['sin', 'exp'])
+    """
+
+    assert not modes.profiles
+    assert degenerate_mode in ["sin", "exp"]
     R = indexProfile.R
     TH = indexProfile.TH
     a = indexProfile.a
-    
-    logger.info('Finding analytical LP mode profiles associated to the propagation constants.')
+
+    logger.info(
+        "Finding analytical LP mode profiles associated to the propagation constants."
+    )
     t0 = time.time()
 
     # for exp modes, we have +m and -m for degenerate modes
-    if degenerate_mode == 'exp':
-        modes.m = \
-            [m if not (m, l) in zip(modes.m[:idx], modes.l[:idx]) else -m for idx, (m,l) in enumerate(zip(modes.m, modes.l))]
+    if degenerate_mode == "exp":
+        modes.m = [
+            m if not (m, l) in zip(modes.m[:idx], modes.l[:idx]) else -m
+            for idx, (m, l) in enumerate(zip(modes.m, modes.l))
+        ]
 
     # Avoid division bt zero in the Bessel function
-    R[R<np.finfo(np.float32).eps] = np.finfo(np.float32).eps
-    Rlessa = (R <= a)
-    Rgreatera = (R > a)
-    
+    R[R < np.finfo(np.float32).eps] = np.finfo(np.float32).eps
+    Rlessa = R <= a
+    Rgreatera = R > a
+
     modes.profiles = Parallel(n_jobs=n_jobs)(
-        delayed(calc_mode)(modes, idx, degenerate_mode, R, a, TH,
-                           Rlessa, Rgreatera) for idx in range(modes.number))
-    logger.info("Found %g LP mode profiles in %0.1f minutes." % (
-        modes.number, (time.time() - t0) / 60))
+        delayed(calc_mode)(modes, idx, degenerate_mode, R, a, TH, Rlessa, Rgreatera)
+        for idx in range(modes.number)
+    )
+    logger.info(
+        "Found %g LP mode profiles in %0.1f minutes."
+        % (modes.number, (time.time() - t0) / 60)
+    )
 
     return modes
--- pyMMF/solvers/radial_test.py
+++ pyMMF/solvers/radial_test.py
@@ -1,9 +1,9 @@
-'''
+"""
 Solver for axisymmetric index profile defined by a radial function,
 e.g. graded index fibers.
 Solve the 1D problem using the finite difference recursive scheme for 
 Riccati's equations.
-'''
+"""
 
 import numpy as np
 from scipy.interpolate import interp1d
@@ -13,6 +13,7 @@ import time
 
 from ..modes import Modes
 from ..logger import get_logger
+
 logger = get_logger(__name__)
 
 MIN_RADIUS_BC_DEFAULT = 1.5
@@ -20,115 +21,126 @@ CHANGE_BC_RADIUS_STEP_DEFAULT = 0.9
 N_BETA_COARSE_DEFAULT = int(1e3)
 
 # choice for degenerate subspaces
-EXP_PHASE_FUNCS = [lambda x: np.exp(1j*x), lambda x: np.exp(-1j*x)]
+EXP_PHASE_FUNCS = [lambda x: np.exp(1j * x), lambda x: np.exp(-1j * x)]
 SIN_PHASE_FUNCS = [np.sin, np.cos]
 
 
 class PrecisionError(Exception):
     def __init__(self, min_val, max_val):
-        self.msg = f'Stagnation due to floating point precision in the interval ({min_val}, {max_val})'
+        self.msg = f"Stagnation due to floating point precision in the interval ({min_val}, {max_val})"
         logger.error(self.msg)
         super().__init__(self.msg)
-        
+
 
 class BisectRootValueError(Exception):
     def __init__(self, root, fval, ftol):
-        self.msg = f'Field limit {fval} at the founded beta={root} is greater than field_limit_tol={ftol}'
+        self.msg = f"Field limit {fval} at the founded beta={root} is greater than field_limit_tol={ftol}"
         logger.error(self.msg)
-        super().__init__(self.msg)    
-        
+        super().__init__(self.msg)
+
 
 class BisectNotConvergedError(Exception):
     def __init__(self, beta, fval, binfo):
-        self.msg = f'Binary search was not converged with beta={beta}, f={fval}\n{binfo}'
+        self.msg = (
+            f"Binary search was not converged with beta={beta}, f={fval}\n{binfo}"
+        )
         logger.error(self.msg)
         super().__init__(self.msg)
 
 
 class SmallRmaxError(Exception):
     def __init__(self, r_max, min_radius_bc):
-        self.msg = \
-            f'Boundary condition could not be met for r_max < {min_radius_bc}a\n' + \
-                '\tTry lower your tolerence or change the resolution.'
+        self.msg = (
+            f"Boundary condition could not be met for r_max < {min_radius_bc}a\n"
+            + "\tTry lower your tolerence or change the resolution."
+        )
         logger.error(self.msg)
         super().__init__(self.msg)
 
 
 class CalculationStopException(Exception):
     def __init__(self):
-        self.msg = 'Calculation stops'
+        self.msg = "Calculation stops"
         logger.error(self.msg)
         super().__init__(self.msg)
 
 
-@jit('float64[:](int8,float32,float32[:],float32[:],float64, float64, float64)', nopython=True, fastmath=True)
+@jit(
+    "float64[:](int8,float32,float32[:],float32[:],float64, float64, float64)",
+    nopython=True,
+    fastmath=True,
+)
 def _get_field_fast(m, dh, r, nr, beta_min, delta_beta, k0):
+    beta_sq = beta_min**2 + delta_beta**2 + 2 * beta_min * delta_beta
 
-    beta_sq = beta_min**2+delta_beta**2+2*beta_min*delta_beta
     def Q(r):
         return 1 / r
 
-    def P(r,n):
-        return (k0 * n)**2 - beta_sq - (m / r)**2
+    def P(r, n):
+        return (k0 * n) ** 2 - beta_sq - (m / r) ** 2
 
-    def iter_g(gn,r,n):
-        Qh = dh*Q(r)/2
-        return (- (dh**2*P(r,n) - 2 + (1 - Qh) / (1 + dh*gn)) / (1 + Qh) - 1) / dh
+    def iter_g(gn, r, n):
+        Qh = dh * Q(r) / 2
+        return (-(dh**2 * P(r, n) - 2 + (1 - Qh) / (1 + dh * gn)) / (1 + Qh) - 1) / dh
 
     # Boundary conditions at r~0
     if m == 0:
-        g0 = [0.]
-        f0 = [1.]
+        g0 = [0.0]
+        f0 = [1.0]
     else:
-        g0 = [0., (1-dh**2*P(r[1],nr[1]))/dh]
-        f0 = [0., dh]
+        g0 = [0.0, (1 - dh**2 * P(r[1], nr[1])) / dh]
+        f0 = [0.0, dh]
 
     bc_length = len(f0)
 
-    f_vec = np.empty(shape = r.shape, dtype=np.float64)
+    f_vec = np.empty(shape=r.shape, dtype=np.float64)
     f_vec[:bc_length] = f0
-    
-    g_vec = np.empty(shape = r.shape, dtype=np.float64)
+
+    g_vec = np.empty(shape=r.shape, dtype=np.float64)
     g_vec[:bc_length] = g0
-    
+
     for ir in range(bc_length, len(r)):
-        g_vec[ir] = iter_g(g_vec[ir-1], r[ir], nr[ir])
-        f_vec[ir] = f_vec[ir-1] * (1 + dh*g_vec[ir-1])
+        g_vec[ir] = iter_g(g_vec[ir - 1], r[ir], nr[ir])
+        f_vec[ir] = f_vec[ir - 1] * (1 + dh * g_vec[ir - 1])
     return f_vec
 
 
 def get_field_fast(m, dh, r, nr, beta_min, delta_beta, k0):
-    '''
+    """
     Get the field calulcated using the recursive scheme for the quadratic Ricatti
     equation as [1].
-    
+
     Be careful that numba does not allow float128, which limits the precision
     and can lead to stagnation of the recursive iterations.
-    
-    .. [1] Lakshman S. Tamil, S. S. Mitra, R. Dutta, and J. M. T. Pereira, 
-       "Finite difference solution for graded-index cylindrical dielectric waveguides: 
+
+    .. [1] Lakshman S. Tamil, S. S. Mitra, R. Dutta, and J. M. T. Pereira,
+       "Finite difference solution for graded-index cylindrical dielectric waveguides:
        a scalar wave approximation" Applied Optics, vol. 30,
        pp. 1113-1116, 1991.
-    '''
-    return _get_field_fast(np.int8(m),
-                      np.float32(dh),
-                      r.astype(np.float32), 
-                      nr.astype(np.float32), 
-                      np.float64(beta_min),
-                      np.float64(delta_beta),
-                      np.float64(k0)
-                     )
+    """
+    return _get_field_fast(
+        np.int8(m),
+        np.float32(dh),
+        r.astype(np.float32),
+        nr.astype(np.float32),
+        np.float64(beta_min),
+        np.float64(delta_beta),
+        np.float64(k0),
+    )
 
 
-def scan_betas(m,dh,r,nr, beta_min, delta_betas,k0):
-    '''
+def scan_betas(m, dh, r, nr, beta_min, delta_betas, k0):
+    """
     rough scan of betas values between beta_min and beta_max
     returns the sign of the value of the function for the
     farthest radial point for each beta value.
     (sign changes indicate the presence of an eigenvector)
-    '''
+    """
     # print((m,dh,k0))
-    return [np.sign(get_field_fast(m,dh,r,nr,beta_min, delta_beta,k0)[-1]) for delta_beta in delta_betas]
+    return [
+        np.sign(get_field_fast(m, dh, r, nr, beta_min, delta_beta, k0)[-1])
+        for delta_beta in delta_betas
+    ]
 
 
 def binary_search(func, min_val, max_val, sign, beta_tol=1e-12, field_limit_tol=1e-3):
@@ -140,11 +152,15 @@ def binary_search(func, min_val, max_val, sign, beta_tol=1e-12, field_limit_tol=
     converged = 0
     while not converged:
         try:
-            beta, binfo = bisect(func, min_val, max_val, xtol=beta_tol, full_output=True)
+            beta, binfo = bisect(
+                func, min_val, max_val, xtol=beta_tol, full_output=True
+            )
         except ValueError:
             max_val *= max_val_incr_factor
-            logger.error(f'Field at min_val={min_val} and max_val={max_val} have the same sign.')
-            logger.warning(f'max_val is increased with factor {max_val_incr_factor}')
+            logger.error(
+                f"Field at min_val={min_val} and max_val={max_val} have the same sign."
+            )
+            logger.warning(f"max_val is increased with factor {max_val_incr_factor}")
             continue
         converged = binfo.converged
 
@@ -152,40 +168,36 @@ def binary_search(func, min_val, max_val, sign, beta_tol=1e-12, field_limit_tol=
     if np.abs(fval) > field_limit_tol:
         raise BisectRootValueError(beta, fval, field_limit_tol)
 
-
     binfo.fval = fval
     return beta, binfo
 
 
-def solve_radial_test(
-    indexProfile,
-    wl,
-    **options
-):
-    
+def solve_radial_test(indexProfile, wl, **options):
     t0 = time.time()
-    
-    degenerate_mode = options.get('degenerate_mode','sin')
-    phi_funcs = EXP_PHASE_FUNCS if degenerate_mode == 'exp' else SIN_PHASE_FUNCS
-    min_radius_bc = options.get('min_radius_bc', MIN_RADIUS_BC_DEFAULT)
-    change_bc_radius_step = options.get('change_bc_radius_step', CHANGE_BC_RADIUS_STEP_DEFAULT)
-    N_beta_coarse = options.get('N_beta_coarse', N_BETA_COARSE_DEFAULT)
-    r_max = options.get('r_max', np.max(indexProfile.R))
-    dh = options.get('dh', indexProfile.areaSize/indexProfile.npoints)
-    beta_tol = options.get('beta_tol', np.finfo(np.float64).eps)
-    field_limit_tol = options.get('field_limit_tol', 1e-3)
+
+    degenerate_mode = options.get("degenerate_mode", "sin")
+    phi_funcs = EXP_PHASE_FUNCS if degenerate_mode == "exp" else SIN_PHASE_FUNCS
+    min_radius_bc = options.get("min_radius_bc", MIN_RADIUS_BC_DEFAULT)
+    change_bc_radius_step = options.get(
+        "change_bc_radius_step", CHANGE_BC_RADIUS_STEP_DEFAULT
+    )
+    N_beta_coarse = options.get("N_beta_coarse", N_BETA_COARSE_DEFAULT)
+    r_max = options.get("r_max", np.max(indexProfile.R))
+    dh = options.get("dh", indexProfile.areaSize / indexProfile.npoints)
+    beta_tol = options.get("beta_tol", np.finfo(np.float64).eps)
+    field_limit_tol = options.get("field_limit_tol", 1e-3)
     save_radial = options.get("save_radial", False)
-    
-    k0 = 2.*np.pi/wl
+
+    k0 = 2.0 * np.pi / wl
 
     n_func = indexProfile.radialFunc
     radius = indexProfile.a
 
     r = np.arange(0, r_max + dh, dh).astype(np.float64)
 
-    beta_min = k0*n_func(r_max)
-    beta_max =  k0*n_func(0)
-    delta_betas = np.linspace(0, beta_max-beta_min, N_beta_coarse)
+    beta_min = k0 * n_func(r_max)
+    beta_max = k0 * n_func(0)
+    delta_betas = np.linspace(0, beta_max - beta_min, N_beta_coarse)
 
     modes = Modes()
     modes.wl = wl
@@ -194,64 +206,63 @@ def solve_radial_test(
     m = 0
 
     while True:
-
-        #logger.info(f'Searching radial modes for m = {m}')
+        # logger.info(f'Searching radial modes for m = {m}')
         if m == 0:
-            r = r-dh/2
+            r = r - dh / 2
 
         nr = np.array([n_func(rn) for rn in r], dtype=np.float64)
 
-        # rough scan of beta values to find the changes of the sign of 
+        # rough scan of beta values to find the changes of the sign of
         # the farthest radial point
-        sign_f = scan_betas(m = m,
-                            dh = dh,
-                            r = r,
-                            nr = nr,
-                            beta_min = beta_min,
-                            delta_betas = delta_betas,
-                            k0 = k0
-                           )
+        sign_f = scan_betas(
+            m=m, dh=dh, r=r, nr=nr, beta_min=beta_min, delta_betas=delta_betas, k0=k0
+        )
 
         # finds where fN change sign
         zero_crossings = np.where(np.diff(sign_f))[0][::-1]
-        logger.info(f'Found {len(zero_crossings)} radial mode(s) for m={m}')
+        logger.info(f"Found {len(zero_crossings)} radial mode(s) for m={m}")
         if len(zero_crossings) == 0:
             break
 
         for l, iz in enumerate(zero_crossings):
-
-            logger.info(f'Searching propagation constant for |l| = {l+1}')
-            # find the beta value that satisfies the best the boundary condition 
+            logger.info(f"Searching propagation constant for |l| = {l+1}")
+            # find the beta value that satisfies the best the boundary condition
             while True:
-
-                if r_max < min_radius_bc*radius:
+                if r_max < min_radius_bc * radius:
                     raise SmallRmaxError(r_max, min_radius_bc)
 
-                r_search = r[r<=r_max]
-                n_search = nr[r<=r_max]
+                r_search = r[r <= r_max]
+                n_search = nr[r <= r_max]
 
                 try:
                     #'Searching for beta value that satisfies the zero condition at r={r_max/radius:.3f}a'
                     def func_fast(delta_beta):
-                        f = get_field_fast(m,dh,r_search,n_search,beta_min, delta_beta,k0)
-                        return f[-1]/np.max(np.abs(f))
-
+                        f = get_field_fast(
+                            m, dh, r_search, n_search, beta_min, delta_beta, k0
+                        )
+                        return f[-1] / np.max(np.abs(f))
 
                     delta_beta, binfo = binary_search(
                         func_fast,
                         min_val=delta_betas[iz],
-                        max_val=delta_betas[iz+1],
+                        max_val=delta_betas[iz + 1],
                         sign=sign_f[iz],
                         beta_tol=beta_tol,
-                        field_limit_tol=field_limit_tol
+                        field_limit_tol=field_limit_tol,
                     )
                     if not binfo.converged:
-                        raise BisectNotConvergedError(delta_beta, func_fast(delta_beta), binfo)
+                        raise BisectNotConvergedError(
+                            delta_beta, func_fast(delta_beta), binfo
+                        )
                     # get the discretized radial function
-                    f_vec = get_field_fast(m,dh,r_search,n_search,beta_min, delta_beta,k0)
+                    f_vec = get_field_fast(
+                        m, dh, r_search, n_search, beta_min, delta_beta, k0
+                    )
                     # get the radial function by interpolation
                     # assume f = 0 for r>r_max
-                    f_interp = np.pad(f_vec, (0,len(r)-len(f_vec)),'constant', constant_values = 0)
+                    f_interp = np.pad(
+                        f_vec, (0, len(r) - len(f_vec)), "constant", constant_values=0
+                    )
                     # assume f = 0 for r>radius * min_radius_bc
                     # f_interp[r > radius * 1] = 0
                     f = interp1d(
@@ -259,36 +270,42 @@ def solve_radial_test(
                     )
                     break
                 except (BisectNotConvergedError, PrecisionError, BisectRootValueError):
-                    logger.warning('Boundary condition could not be met.')
+                    logger.warning("Boundary condition could not be met.")
                     r_max *= change_bc_radius_step
-                    logger.warning(f'Retrying by changing r_max to {r_max/radius:.2f}a')
+                    logger.warning(f"Retrying by changing r_max to {r_max/radius:.2f}a")
                 except Exception as E:
-                    logger.error(f'Unknown exception: {E}')
+                    logger.error(f"Unknown exception: {E}")
                     raise CalculationStopException
 
             mode_profile = f(indexProfile.R)
 
             # add mode
             if m == 0:
-                modes.betas.append(delta_beta+beta_min)
+                modes.betas.append(delta_beta + beta_min)
                 modes.m.append(m)
-                modes.l.append(l+1)
-                modes.number+=1
+                modes.l.append(l + 1)
+                modes.number += 1
                 modes.profiles.append(mode_profile.ravel())
-                modes.profiles[-1] = modes.profiles[-1]/np.sqrt(np.sum(np.abs(modes.profiles[-1])**2))
+                modes.profiles[-1] = modes.profiles[-1] / np.sqrt(
+                    np.sum(np.abs(modes.profiles[-1]) ** 2)
+                )
                 # is the mode a propagative one?
                 modes.propag.append(True)
 
                 if save_radial:
                     modes.data.append({"radial_func": f, "r_max": r_max})
             else:
-                for s, phi_func in zip([-1,1],phi_funcs):
-                    modes.betas.append(delta_beta+beta_min)
-                    modes.m.append(s*m if degenerate_mode == 'exp' else m)
-                    modes.l.append(l+1)
-                    modes.number+=1
-                    modes.profiles.append(mode_profile.ravel()*phi_func(m*indexProfile.TH.ravel()))
-                    modes.profiles[-1] = modes.profiles[-1]/np.sqrt(np.sum(np.abs(modes.profiles[-1])**2))
+                for s, phi_func in zip([-1, 1], phi_funcs):
+                    modes.betas.append(delta_beta + beta_min)
+                    modes.m.append(s * m if degenerate_mode == "exp" else m)
+                    modes.l.append(l + 1)
+                    modes.number += 1
+                    modes.profiles.append(
+                        mode_profile.ravel() * phi_func(m * indexProfile.TH.ravel())
+                    )
+                    modes.profiles[-1] = modes.profiles[-1] / np.sqrt(
+                        np.sum(np.abs(modes.profiles[-1]) ** 2)
+                    )
                     # is the mode a propagative one?
                     modes.propag.append(True)
 
@@ -298,6 +315,8 @@ def solve_radial_test(
         m += 1
         r_max = np.max(indexProfile.R)
     # sort the modes
-    modes.sort()  
-    logger.info("Solver found %g modes is %0.2f seconds." % (modes.number,time.time()-t0))
+    modes.sort()
+    logger.info(
+        "Solver found %g modes is %0.2f seconds." % (modes.number, time.time() - t0)
+    )
     return modes
