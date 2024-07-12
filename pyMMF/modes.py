87b18af45f36a04365c2f620970f75f5c94c8497
Sébastien M. Popoff <sebastien.popoff@espci.fr>
2024-07-12 22:49:45 +0200

    Added PEP 484 type hints and updated docstrings in modes.py.

--- pyMMF/modes.py
+++ pyMMF/modes.py
@@ -161,31 +161,26 @@ class Modes:
 
         One can add the effect of curvature to a system solved for a straight fiber.
         It returns then the evolution operator in the basis of the straight fiber modes.
-        The calculation is different from directly solving the system for a bent fiber [#]_ [#]_.
+        The calculation is different from directly solving the system for a bent fiber [#] [#].
+        `PEP 484`_
 
 
 
-        Parameters
-        ----------
-        npola : int (1 or 2)
-            number of polarizations considered.
-            defaults to 1
+        Args:
+            npola (int, optional): Number of polarizations. Defaults value: 1.
+            curvature (float or None, optional): The curvature of the fiber. Defaults: None.
 
-        curvature: float (optional)
-            curvature (in microns)
-            defaults to None
+        Returns:
+        B (numpy array): The propagation operator
 
-        Returns
-        -------
-        B : numpy array
-            The propagation operator
+        .. _Google Python Style Guide:
+           http://google.github.io/styleguide/pyguide.html
 
-        See Also
-        --------
-            getPropagationMatrix()
+        See Also:
+           getPropagationMatrix()
 
-        Notes
-        -----
+        .. _PEP 484:
+           https://www.python.org/dev/peps/pep-0484/
 
         .. [#]  M. Plöschner, T. Tyc and T. Čižmár, "Seeing through chaos in multimode fibres"
                 Nature Photonics, vol. 9,
@@ -299,10 +294,10 @@ class Modes:
         ----------
 
         distance : float
-                        size of the fiber segment (in microns)
+            size of the fiber segment (in microns)
 
         npola : int (1 or 2)
-                        number of polarizations considered. For npola = 2, the mode matrix will be a block diagonal matrix.
+            number of polarizations considered. For npola = 2, the mode matrix will be a block diagonal matrix.
 
         curvature : float
             curvature of the fiber segment (in microns)
@@ -311,7 +306,7 @@ class Modes:
         -------
 
             T : numpy array
-                    The transmission matrix of the fiber.
+                The transmission matrix of the fiber.
 
         See Also
         --------
@@ -403,3 +398,93 @@ class Modes:
         obj = cls()
         obj.load(filename)
         return obj
+
+    def doc_test(self, param1, param2):
+        r"""Summarize the function in one line.
+
+        Several sentences providing an extended description. Refer to
+        variables using back-ticks, e.g. `var`.
+
+        Parameters
+        ----------
+        var1 : array_like
+            Array_like means all those objects -- lists, nested lists, etc. --
+            that can be converted to an array.  We can also refer to
+            variables like `var1`.
+        var2 : int
+            The type above can either refer to an actual Python type
+            (e.g. ``int``), or describe the type of the variable in more
+            detail, e.g. ``(N,) ndarray`` or ``array_like``.
+        *args : iterable
+            Other arguments.
+        long_var_name : {'hi', 'ho'}, optional
+            Choices in brackets, default first when optional.
+
+        Returns
+        -------
+        type
+            Explanation of anonymous return value of type ``type``.
+        describe : type
+            Explanation of return value named `describe`.
+        out : type
+            Explanation of `out`.
+        type_without_description
+
+        Other Parameters
+        ----------------
+        only_seldom_used_keyword : int, optional
+            Infrequently used parameters can be described under this optional
+            section to prevent cluttering the Parameters section.
+        **kwargs : dict
+            Other infrequently used keyword arguments. Note that all keyword
+            arguments appearing after the first parameter specified under the
+            Other Parameters section, should also be described under this
+            section.
+
+        Raises
+        ------
+        BadException
+            Because you shouldn't have done that.
+
+        See Also
+        --------
+        numpy.array : Relationship (optional).
+        numpy.ndarray : Relationship (optional), which could be fairly long, in
+                        which case the line wraps here.
+        numpy.dot, numpy.linalg.norm, numpy.eye
+
+        Notes
+        -----
+        Notes about the implementation algorithm (if needed).
+
+        This can have multiple paragraphs.
+
+        You may include some math:
+
+        .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
+
+        And even use a Greek symbol like :math:`\omega` inline.
+
+        References
+        ----------
+        Cite the relevant literature, e.g. [1]_.  You may also cite these
+        references in the notes section above.
+
+        .. [1] O. McNoleg, "The integration of GIS, remote sensing,
+        expert systems and adaptive co-kriging for environmental habitat
+        modelling of the Highland Haggis using object-oriented, fuzzy-logic
+        and neural-network techniques," Computers & Geosciences, vol. 22,
+        pp. 585-588, 1996.
+
+        Examples
+        --------
+        These are written in doctest format, and should illustrate how to
+        use the function.
+
+        >>> a = [1, 2, 3]
+        >>> print([x + 3 for x in a])
+        [4, 5, 6]
+        >>> print("a\nb")
+        a
+        b
+        """
