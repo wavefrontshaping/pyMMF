26bc4b5dc5d2644218df5505ae65a31e11b91977
Sébastien M. Popoff <sebastien.popoff@espci.fr>
2024-09-13 15:48:49 +0200

    add __all__

--- pyMMF/__init__.py
+++ pyMMF/__init__.py
@@ -6,3 +6,12 @@ from .core import (
 from .index_profile import IndexProfile
 from .modes import Modes
 from .TM import TransmissionMatrix
+
+__all__ = [
+    "propagationModeSolver",
+    "estimateNumModesSI",
+    "estimateNumModesGRIN",
+    "IndexProfile",
+    "Modes",
+    "TransmissionMatrix",
+]
